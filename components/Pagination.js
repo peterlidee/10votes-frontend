84eb89b2cfd2b77a093756fb3149427e6feb8f60
peter@lidee.be <peter@lidee.be>
2020-08-06 17:22:04 +0200

    styled and cleaned pagination
    "styling pagination"

--- components/Pagination.js
+++ components/Pagination.js
@@ -3,107 +3,105 @@ import Link from 'next/link';
 import { perPage } from '../config';
 import getRouterData from '../lib/getRouterData';
 
-
 const Pagination = props => {
     
     const routerData = getRouterData(true);
     //console.log('routerdata', routerData)
 
     return(
-        <div>
-            <Query 
-                query={ routerData.query } 
-                variables={ routerData.variables }
-            >
-                {({ data, loading, error })=> {
-                    if(loading || error || !data) return null;
-                    const itemsCount = data.itemsConnection.aggregate.count;
-                    const totalPages = Math.ceil(itemsCount / perPage);
-                    
-                    // no pagination when only one page
-                    if(itemsCount - perPage <= 0) return null; 
-                    
-                    // TODO build long pagination
-                    if(totalPages > 10) return <p>Long pagination</p>; 
+        <Query 
+            query={ routerData.query } 
+            variables={ routerData.variables }
+        >
+            {({ data, loading, error })=> {
+                if(loading || error || !data) return null;
+                const itemsCount = data.itemsConnection.aggregate.count;
+                const totalPages = Math.ceil(itemsCount / perPage);
+                
+                // no pagination when only one page
+                if(itemsCount - perPage <= 0) return null; 
+                
+                // TODO build long pagination
+                if(totalPages > 10) return <p>Long pagination</p>; 
 
-                    // construct the pagination
-                    let pageLinks = [];
-                    for(let i = 1; i <= totalPages; i++){
-                        pageLinks.push(i)
-                    }
+                // construct the pagination
+                let pageLinks = [];
+                for(let i = 1; i <= totalPages; i++){
+                    pageLinks.push(i)
+                }
 
-                    return(
-                        <div>
-                            {routerData.page > 1 &&
-                                <Link
-                                    href={{
-                                        pathname: routerData.hrefPath,
-                                        query: {
-                                            orderBy: routerData.orderBy,
-                                            page: routerData.page - 1,
-                                        }
-                                    }}
-                                    as={{
-                                        pathname: routerData.asPath,
-                                        query: {
-                                            orderBy: routerData.orderBy,
-                                            page: routerData.page - 1,
-                                        }
-                                    }}
-                                >
-                                    <a className="pagination-link pagination-link--prevnext">prev</a>
-                                </Link>
-                            }
-                            {pageLinks.map((pageLink) => (
-                                <span key={`pagelink-${pageLink}`}>
-                                    {pageLink === routerData.page && <span className="pagination-link pagination-link--current">{pageLink}</span>}
-                                    {pageLink !== routerData.page && 
-                                        <Link
-                                            href={{
-                                                pathname: routerData.hrefPath,
-                                                query: {
-                                                    orderBy: routerData.orderBy,
-                                                    page: pageLink,
-                                                }
-                                            }}
-                                            as={{
-                                                pathname: routerData.asPath,
-                                                query: {
-                                                    orderBy: routerData.orderBy,
-                                                    page: pageLink,
-                                                }
-                                            }}
-                                        >
-                                            <a className="pagination-link">{pageLink}</a>
-                                        </Link>
+                return(
+                    <div className="items__pagination">
+                        {routerData.page > 1 &&
+                            <Link
+                                href={{
+                                    pathname: routerData.hrefPath,
+                                    query: {
+                                        orderBy: routerData.orderBy,
+                                        page: routerData.page - 1,
                                     }
-                                </span>
-                            ))}
-                            {(routerData.page !== totalPages) &&
-                                <Link
-                                    href={{
-                                        pathname: routerData.hrefPath,
-                                        query: {
-                                            orderBy: routerData.orderBy,
-                                            page: routerData.page + 1,
-                                        }
-                                    }}
-                                    as={{
-                                        pathname: routerData.asPath,
-                                        query: {
-                                            orderBy: routerData.orderBy,
-                                            page: routerData.page + 1,
-                                        }
-                                    }}
-                                >
-                                    <a className="pagination-link pagination-link--prevnext">next</a>
-                                </Link>
-                            }
-                        </div>
-                    )
-                }}
-            </Query>
-        </div>
+                                }}
+                                as={{
+                                    pathname: routerData.asPath,
+                                    query: {
+                                        orderBy: routerData.orderBy,
+                                        page: routerData.page - 1,
+                                    }
+                                }}
+                            >
+                                <a className="pagination__link pagination__link--prevnext">prev</a>
+                            </Link>
+                        }
+                        {pageLinks.map((pageLink) => (
+                            <span key={`pagelink-${pageLink}`}>
+                                {pageLink === routerData.page && 
+                                    <span className="pagination__link pagination__link--current">{pageLink}</span>}
+                                {pageLink !== routerData.page && 
+                                    <Link
+                                        href={{
+                                            pathname: routerData.hrefPath,
+                                            query: {
+                                                orderBy: routerData.orderBy,
+                                                page: pageLink,
+                                            }
+                                        }}
+                                        as={{
+                                            pathname: routerData.asPath,
+                                            query: {
+                                                orderBy: routerData.orderBy,
+                                                page: pageLink,
+                                            }
+                                        }}
+                                    >
+                                        <a className="pagination__link">{pageLink}</a>
+                                    </Link>
+                                }
+                            </span>
+                        ))}
+                        {(routerData.page < totalPages) &&
+                            <Link
+                                href={{
+                                    pathname: routerData.hrefPath,
+                                    query: {
+                                        orderBy: routerData.orderBy,
+                                        page: routerData.page + 1,
+                                    }
+                                }}
+                                as={{
+                                    pathname: routerData.asPath,
+                                    query: {
+                                        orderBy: routerData.orderBy,
+                                        page: routerData.page + 1,
+                                    }
+                                }}
+                            >
+                                <a className="pagination__link pagination__link--prevnext">next</a>
+                            </Link>
+                        }
+                    </div>
+                )
+            }}
+        </Query>
     )
 }
 
