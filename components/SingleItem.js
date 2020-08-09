673f30af8d57b6c93052a454c7d6a5c7eea0509e
peter@lidee.be <peter@lidee.be>
2020-08-09 18:14:54 +0200

    added styling to SingleItem

--- components/SingleItem.js
+++ components/SingleItem.js
@@ -1,8 +1,10 @@
 import { Query } from "react-apollo";
 import gql from 'graphql-tag';
 import Link from 'next/link';
+import PropTypes from 'prop-types';
 
-import Error from './Error';
+import Loader from './snippets/Loader';
+import NewError from './NewError';
 import MetaTitle from './snippets/MetaTitle';
 import Voting from './voting/Voting';
 
@@ -26,59 +28,65 @@ const SINGLE_ITEM_QUERY = gql`
                 id
                 slug
             }
+            voteCount
         }
     }
 `;
 
-class SingleItem extends React.Component{
-    render(){
-        return(
-            <Query query={ SINGLE_ITEM_QUERY } variables={{ itemId: this.props.id }}>
-                {( { error, loading, data } ) => {
+const SingleItem  = props => (
+    <div className="grid-single-item">
+        <Query query={ SINGLE_ITEM_QUERY } variables={{ itemId: props.id }}>
+            {( { error, loading, data } ) => {
 
-                    if(loading) return <p>...loading</p>
-                    if(error) return <Error error={error} />
-                    if(!data.item) return <p>Uhm, something went wrong. Try again?</p>
-                    const {item} = data;
+                if(loading) return <Loader containerClass="items-loader" />
+                if(error) return <NewError error={error} />
+                if(!data || !data.item) return <p className="no-data">Uhm, something went wrong. Try again?</p>
+                const {item} = data;
 
-                    // construct a string we will use as title and alt: "Image in {location} {tags}"
-                    const tagsString = item.tags ? item.tags.map(tag => `#${tag.name}`).join(" ") : '';
-                    const description = `Image in ${item.location.name} ${tagsString}`;
+                // construct a string we will use as title and alt: "Image in {location} {tags}"
+                const tagsString = item.tags ? item.tags.map(tag => `#${tag.name}`).join(" ") : '';
+                const description = `Pic in ${item.location.name} ${tagsString}`;
 
-                    return(
-                        <div>
-                            <MetaTitle>{description}</MetaTitle>
-                            <img src={item.largeImage} alt={description} width="150" />
-                            <div>
-                                location: 
-                                <Link 
-                                    href="/location/[countryCode]/[place]" 
-                                    as={`/location/${data.item.location.country.countryCode}/${data.item.location.slug}`}
-                                >
-                                    <a>{item.location.name} - {item.location.country.name}</a>
-                                </Link>
+                return(
+                    <article className="item item--single">
+                        <MetaTitle>{description}</MetaTitle>
+                        <img src={item.largeImage} alt={description} className="item__image item__image--large" />
+
+                        <div className="item__meta">
+                            <div className="item__tags">
+                                {item.location && 
+                                    <Link 
+                                        href="/location/[countryCode]/[place]" 
+                                        as={`/location/${item.location.country.countryCode}/${item.location.slug}`}
+                                    >
+                                        <a className="item__tag item__tag--location">{item.location.name}</a>
+                                    </Link>
+                                }
+                                {item.tags.length > 0 &&
+                                    <>
+                                        {item.tags.map(tag => (
+                                            <Link key={tag.id} href="/tags/[tagslug]" as={`/tags/${tag.slug}`}>
+                                                <a className="item__tag item__tag--tag">{tag.name}</a>
+                                            </Link>
+                                        ))}
+                                    </> 
+                                }
                             </div>
-                            {item.tags.length > 0 &&
-                                <div>
-                                    tags:
-                                    {item.tags.map(tag => (
-                                        <Link 
-                                            href="/tags/[tagslug]"
-                                            as={`/tags/${tag.slug}`}
-                                            key={tag.id}
-                                        >
-                                            <a>#{tag.name}</a>
-                                        </Link>                                    
-                                    ))}
-                                </div>
-                            }
+                            <div className="item__votes">
+                                <div className="item__votes__number">{item.voteCount}</div>
+                                <div className="item__votes__label">votes</div>
+                            </div>
                             <Voting currentItemId={item.id} />
                         </div>
-                    )
-                }}
-            </Query>
-        );
-    }
-}
+                    </article>
+                )
+            }}
+        </Query>
+    </div>
+);
+
+SingleItem.propTypes = {
+    id: PropTypes.string.isRequired,
+};
 
 export default SingleItem;
