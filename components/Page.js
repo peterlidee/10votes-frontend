46ad26eaf6c5f81602ed58c0867e2b7b326beac4
peter@lidee.be <peter@lidee.be>
2020-11-16 22:39:22 +0100

    new test

--- components/Page.js
+++ components/Page.js
@@ -2,6 +2,8 @@ import Meta from './header/Meta';
 import Header from './header/Header';
 import Footer from './footer/Footer.js';
 
+import User from '../account/User';
+
 class Page extends React.Component{
     render(){
         return(
@@ -9,6 +11,17 @@ class Page extends React.Component{
                 {/*<Meta />*/}
                 {/*<Header />*/}
                 <main className="site__main">
+                    {/* test */}
+                    <User>
+                        {({ loading, error, data }) => {
+                            if(error) return <p>Error</p>
+                            if(loading) return <p>loading</p>
+                            if(!data) return <p>no data</p>
+                            console.log('data', data)
+                            return <p>Hello from inside user data.</p>
+                        }}
+                    </User>
+                    {/* test */}
                     hello
                     {/*this.props.children*/}
                 </main>
