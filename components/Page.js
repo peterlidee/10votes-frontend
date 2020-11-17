08ad512631efd8ade6dd26fcc7980b8089c63a1d
peter@lidee.be <peter@lidee.be>
2020-11-17 22:49:43 +0100

    rewrote page as standalone

--- components/Page.js
+++ components/Page.js
@@ -1,11 +1,11 @@
-import Meta from './header/Meta';
-import Header from './header/Header';
-import Footer from './footer/Footer.js';
+// import Meta from './header/Meta';
+// import Header from './header/Header';
+// import Footer from './footer/Footer.js';
 
-import User from './account/User';
-import User2 from './account/User2'
+// import User from './account/User';
+// import User2 from './account/User2';
 
-class Page extends React.Component{
+class PageX extends React.Component{
     render(){
         return(
             <>
@@ -25,5 +25,47 @@ class Page extends React.Component{
         )
     }
 }
+
+
+
+
+
+
+
+import { Query } from 'react-apollo';
+import gql from 'graphql-tag';
+
+const GET_TAG_QUERY = gql`
+    query GET_TAG_QUERY($name: String!){
+        tag(where: {name: $name}){
+            id
+            name
+        }
+    }
+`;
+
+
+
+const Page = props => (
+    <div>
+        <Query query={GET_TAG_QUERY} variables={{ name: "test" }}>
+            {({loading, error, data}) => {
+                if(loading) return <p>loading</p>
+                if(error) return <p>{error.message}</p>
+                if(!data){
+                    console.log('no data found', data)
+                }
+                console.log('there was data', data)
+                return <p>Hello</p>
+            }}
+        </Query>
+    </div>
+);
+
+
+
+
+
+
 
 export default Page;
