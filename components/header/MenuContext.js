4bc953c62341a631c5c37c9e9daaa12528498a1c
peter@lidee.be <peter@lidee.be>
2020-11-29 18:12:01 +0100

    placed menucontext into its own component

--- components/header/MenuContext.js
+++ components/header/MenuContext.js
@@ -1,10 +1,51 @@
-import React from 'react';
+// testing all router click events
+// we will also need to handle redirects!?? DO WE? TODO
+import Router from 'next/router';
 
 // menu context handles the open or closed state of the menu,
 // get handled in _app.js and called in Nav.js
 const MenuContext = React.createContext({
     menuOpen: true,  
     toggleMenu: () => {},
-});  
+});
 
-export default MenuContext;
+class MenuContextComponent extends React.Component{
+    constructor(props){
+        super(props)
+        this.state = { // we use state to feed MenuContext
+            menuOpen: false,
+            toggleMenu: this.toggleMenu,
+        }
+
+        // add router event,
+        // everytime a link is clicked, close the menu (MenuContext) if it was open
+        Router.events.on('routeChangeStart', this.resetMenu)
+        // and handle possible error
+        Router.events.on('routeChangeError', (err, url) => {
+            if (err.cancelled) {
+                this.resetMenu()
+            }
+        })
+    }
+
+    toggleMenu = () => { // gets called by menu button, changes MenuContext via state
+        this.setState({
+            menuOpen: !this.state.menuOpen
+        });
+    }
+    resetMenu = () => { // gets called on routerchange, changes MenuContext via state
+        if(this.state.menuOpen){
+            this.setState({ menuOpen: false })
+        }
+    }
+    render(){
+        return(
+            <MenuContext.Provider value={this.state}>
+                {this.props.children}
+            </MenuContext.Provider>
+        )
+    }
+}
+
+export default MenuContextComponent;
+export { MenuContext };
