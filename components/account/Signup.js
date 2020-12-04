fca881a5d9073b41441433a0e61bb898af4938e0
peter@lidee.be <peter@lidee.be>
2020-12-04 17:13:47 +0100

    reworked entire signup component with hooks

--- components/account/Signup.js
+++ components/account/Signup.js
@@ -1,5 +1,5 @@
-import { Mutation } from 'react-apollo';
-import gql from 'graphql-tag';
+import {useState} from 'react';
+import { gql, useMutation } from '@apollo/client';
 import { CURRENT_USER_QUERY } from './User';
 import Link from 'next/link';
 
@@ -18,140 +18,125 @@ const SIGNUP_MUTATION = gql`
     }
 `;
 
-class Signup extends React.Component{
-    state = {
-        password: "",
-        email: "",
+function Signup(){
+    // react state hooks
+    const [ email, setEmail ] = useState("");
+    const [ password, setPassword ] = useState("");
+    // apollo mutation hook
+    const [signup, { error, data, loading, called }] = useMutation(SIGNUP_MUTATION);
+
+    if(called && !error && !loading && data && data.signup){
+        return <p className="no-data">You are logged into your new account: {data.signup.email}.</p>
     }
-    saveToState = (e) => {
-        this.setState({
-            [e.target.name]: e.target.value
-        })
+
+    if(error){ // overwrite the bad UX graphQl error
+        if (error.message == "A unique constraint would be violated on User. Details: Field name = email"){
+            const calledEmail = email ? `The email: ${email}` : "This email";
+            error.message = `${calledEmail} is already taken.`;
+        }
     }
-    clearField = (name) => {
-        this.setState({
-            [name]: ""
-        });
-    }
-    render(){
-        return(
-            <Mutation 
-                mutation={SIGNUP_MUTATION} 
-                variables={this.state}
-                refetchQueries={[{ query: CURRENT_USER_QUERY }]}
-            >
-                {(signup, { called, loading, error, data }) => {
-                    if(called && !error && !loading && data && data.signup){
-                        return <p className="no-data">You are logged into your new account: {data.signup.email}.</p>
-                    }
-                    if(error){ // overwrite the bad UX graphQl error
-                        if (error.message == "GraphQL error: A unique constraint would be violated on User. Details: Field name = email"){
-                            console.log('state', this.state.email)
-                            const calledEmail = this.state.email ? `The email: ${this.state.email}` : "This email";
-                            error.message = `${calledEmail} is already taken.`;
-                        }
-                    }
-                    return(
-                        <>
-                            <MetaTitle>Sign up for an account</MetaTitle>
-                            <h2 className="title">Sign up for a new account</h2>
 
-                            <form method="post" className="form-part form-part--account" onSubmit={async e => {
-                                e.preventDefault();
-                                // more form validation here
-                                // this.validateForm()
-                                const res = await signup().catch(error => console.log(error.message));
-                                this.setState({ email: '', password: '' });
-                            }}>
+    return(
+        <>
+            <MetaTitle>Sign up for an account</MetaTitle>
+            <h2 className="title">Sign up for a new account</h2>
 
-                                <FormRow 
-                                    number={1}
-                                    label={{ 
-                                        text: "Email", 
-                                        required: true,
-                                        html: true,
-                                        for: "email",
-                                    }}
-                                    valid={{ 
-                                        field: this.state.email, 
-                                        form: this.state.email,
-                                    }}
-                                >
-                                    <InputContainer 
-                                        clearField={this.clearField} 
-                                        name="email" 
-                                        isEmpty={!this.state.email}
-                                    >
-                                        <input 
-                                            type="email" 
-                                            className="form-part__input"
-                                            name="email"
-                                            id="email"
-                                            value={this.state.email}
-                                            onChange= {this.saveToState}
-                                        />
-                                    </InputContainer>
-                                </FormRow>
+            <form method="post" className="form-part form-part--account" onSubmit={async e => {
+                e.preventDefault();
+                // more form validation here
+                const res = await signup({ 
+                    variables: { email, password },
+                    //refetchQueries: [{ query: CURRENT_USER_QUERY }], // TODO? not needed?
+                }).catch(error => console.log(error.message));
+                // reset the form fields to empty
+                setEmail('');
+                setPassword('');
+            }}>
+                <FormRow 
+                    number={1}
+                    label={{ 
+                        text: "Email", 
+                        required: true,
+                        html: true,
+                        for: "email",
+                    }}
+                    valid={{ 
+                        field: email, 
+                        form: email,
+                    }}
+                >
+                    <InputContainer 
+                        clearField={() => setEmail('')} 
+                        name="email" 
+                        isEmpty={!email}
+                    >
+                        <input 
+                            type="email" 
+                            className="form-part__input"
+                            name="email"
+                            id="email"
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                        />
+                    </InputContainer>
+                </FormRow>
 
-                                <FormRow 
-                                    number={2}
-                                    label={{ 
-                                        text: "Password", 
-                                        required: true,
-                                        html: true,
-                                        for: "password",
-                                    }}
-                                    valid={{ 
-                                        field: this.state.password, 
-                                        form: this.state.email && this.state.password,
-                                    }}
-                                >
-                                    <InputContainer 
-                                        clearField={this.clearField} 
-                                        name="password" 
-                                        isEmpty={!this.state.password}
-                                    >
-                                        <input 
-                                            type="password" 
-                                            minLength="6" 
-                                            className="form-part__input"
-                                            name="password"
-                                            id="password"
-                                            value={this.state.password}
-                                            onChange={this.saveToState} 
-                                        />
-                                    </InputContainer>
-                                </FormRow>
+                <FormRow 
+                    number={2}
+                    label={{ 
+                        text: "Password", 
+                        required: true,
+                        html: true,
+                        for: "password",
+                    }}
+                    valid={{ 
+                        field: password, 
+                        form: email && password,
+                    }}
+                >
+                    <InputContainer 
+                        clearField={() => setPassword('')} 
+                        name="password" 
+                        isEmpty={!password}
+                    >
+                        <input 
+                            type="password" 
+                            minLength="6" 
+                            className="form-part__input"
+                            name="password"
+                            id="password"
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)} 
+                        />
+                    </InputContainer>
+                </FormRow>
 
-                                {error && 
-                                    <FormRow valid={{ error: true, form: this.state.email && this.state.password }}>
-                                        <Error error={error} plain={true} />
-                                    </FormRow>
-                                }
+                {error && 
+                    <FormRow valid={{ error: true, form: email && password }}>
+                        <Error error={error} plain={true} />
+                    </FormRow>
+                }
 
-                                <FormRow 
-                                    number={3}
-                                    extraClass="last"
-                                    valid={{ 
-                                        field: this.state.email && this.state.password, 
-                                        form: this.state.email && this.state.password,
-                                    }}
-                                >
-                                    <FormButton loading={loading} formValid={!this.state.email || !this.state.password}>
-                                        sign up
-                                    </FormButton>
-                                </FormRow>  
-                                
-                                <div className="form-part--account__links">
-                                    <Link href="/login"><a className="form__link">Do you already have an account? Log in.</a></Link>
-                                </div>
-                            </form>
-                        </>
-                    )
-                }}
-            </Mutation>
-        )
-    }
+                <FormRow 
+                    number={3}
+                    extraClass="last"
+                    valid={{ 
+                        field: email && password, 
+                        form: email && password,
+                    }}
+                >
+                    <FormButton loading={loading} formValid={!email || !password}>
+                        sign up
+                    </FormButton>
+                </FormRow>  
+                
+                <div className="form-part--account__links">
+                    <Link href="/login"><a className="form__link">Do you already have an account? Log in.</a></Link>
+                </div>
+
+            </form>
+        </>
+    )
 }
 
 export default Signup;
--- components/formParts/InputContainer.js
+++ components/formParts/InputContainer.js
@@ -1,15 +1,20 @@
 // this component renders container and clear button
 // you pass is an input field as props.children
-// the button needs a clear function, name attribute (the field it is associated with) and an isEmpty as props
+// the button needs 
+// 1. a clear function (f.e. () => setEmail("") )
+// 2. name attribute (the field it is associated with) and an  //TODO???
+// 3. isEmpty as props: if isEmpty, it's hidden cause there is nothing to delete
 
 import PropTypes from 'prop-types';
 
 const InputContainer = props => (
     <div className="input-suggestion__input-container">
         {props.children}
-        <button type="button" className="clear-button" onClick={() => {
-            props.clearField(props.name)
-        }} disabled={props.isEmpty}
+        <button 
+            type="button" 
+            className="clear-button" 
+            onClick={props.clearField}
+            disabled={props.isEmpty}
         >
             &times;
         </button>
