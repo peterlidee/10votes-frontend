c14f9ea2a2fba074fbee0b2ddd246c75d90c9256
peter@lidee.be <peter@lidee.be>
2020-12-10 16:48:07 +0100

    new withApollo and _app

--- pages/_app.js
+++ pages/_app.js
@@ -43,3 +43,15 @@ export default withApollo()(MyApp);
 
 */
 
+import { ApolloProvider } from '@apollo/client'
+import { useApollo } from '../lib/apolloClient'
+
+export default function App({ Component, pageProps }) {
+    const apolloClient = useApollo(pageProps)
+
+    return (
+        <ApolloProvider client={apolloClient}>
+            <Component {...pageProps} />
+        </ApolloProvider>
+    )
+}
