d7a7bdf9f9a5a48ffc9a5b0738e16b9dcd3ea6d4
peter@lidee.be <peter@lidee.be>
2020-12-14 17:49:15 +0100

    temp removed all components as not to have to edited them all

--- components/snippets/icons/IconUpload.js
+++
@@ -1,9 +0,0 @@
-const IconUpload = () => (
-    <svg version="1.1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 15 18" width="15" height="18" className="upload__icon">
-        <polygon points="7.5 0 12 8 3 8 7.5 0"/>
-        <rect x="5.5" y="8" width="4" height="6"/>
-        <rect y="16" width="15" height="2"/>
-    </svg>
-);
-
-export default IconUpload;
