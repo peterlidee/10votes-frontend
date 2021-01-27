772380c2f6c641cf8498a32d4c71a514e1d2ed62
peter@lidee.be <peter@lidee.be>
2021-01-27 15:14:16 +0100

    moved menucontext moved and reworked with hooks

---
+++ components/context/MenuContext.js
@@ -0,0 +1,37 @@
+import { useState } from 'react';
+
+// testing all router click events
+// we will also need to handle redirects!?? DO WE? TODO
+import Router from 'next/router';
+
+// menu context handles the open or closed state of the menu,
+// get handled in _app.js and called in Nav.js
+const MenuContext = React.createContext({
+    menuToggle: false,  
+    setMenuToggle: () => {},
+});
+
+function MenuContextProvider(props){
+    const [menuToggle, setMenuToggle] = useState(false);
+
+    // add router event,
+    // everytime a link is clicked, close the menu (MenuContext) if it was open
+    Router.events.on('routeChangeStart', () => {
+        if(menuToggle) setMenuToggle(false);
+    })
+    // and handle possible error
+    Router.events.on('routeChangeError', (err, url) => {
+        if (err.cancelled) {
+            setMenuToggle(false)
+        }
+    })
+
+    return(
+        <MenuContext.Provider value={{menuToggle, setMenuToggle}}>
+            {props.children}
+        </MenuContext.Provider>
+    )
+}
+
+export default MenuContext;
+export { MenuContextProvider };
