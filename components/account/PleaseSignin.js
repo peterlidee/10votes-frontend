657d1f781b81e24e3fbc5dc9f342f226007eacfb
peter@lidee.be <peter@lidee.be>
2021-01-31 18:22:31 +0100

    moved createItem, reworked PleaseSignin with userContext

--- components/account/PleaseSignin.js
+++ components/account/PleaseSignin.js
@@ -1,25 +1,23 @@
-//import { Query} from 'react-apollo';
-import { CURRENT_USER_QUERY } from './User';
-import Login from './Login';
+import { useContext } from 'react';
+import UserContext from '../context/UserContext';
+
 import Loader from '../snippets/Loader';
 import Error from '../snippets/Error';
+import Login from './Login';
 
-const PleaseSignin = props => (
-    <Query query={ CURRENT_USER_QUERY }>
-        {({data, error, loading}) => {
-            if(loading) return <Loader containerClass="items-loader" />;
-            if(error) return <Error error={error} />
-            if(!data || !data.me){
-                return(
-                    <>
-                        <p className="no-data">You need to be logged in.</p>
-                        <Login />
-                    </>
-                )
-            }
-            return props.children;
-        }}
-    </Query>
-)
+function PleaseSignin(props){
+    const { loading, error, data } = useContext(UserContext);
+    if(loading) return <Loader containerClass="items-loader" />;
+    if(error) return <Error error={error} />
+    if(!data || !data.me){
+        return(
+            <>
+                <p className="no-data">You need to be logged in.</p>
+                <Login />
+            </>
+        )
+    }
+    return props.children;
+}
 
 export default PleaseSignin;
