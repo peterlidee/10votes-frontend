87b6a167c1756988ad62c8baa4b96f1f2d500538
peter@lidee.be <peter@lidee.be>
2021-03-02 17:59:58 +0100

    Added page id query and prepared for SSR

--- pages/update.js
+++ pages/update.js
@@ -1,10 +1,24 @@
-import UpdateItem from '../components/items/UpdateItem';
 import PleaseSignin from '../components/account/PleaseSignin';
+import UpdateItem from '../components/items/UpdateItem';
 
-const Update = props => (
-    <PleaseSignin>
-        <UpdateItem id={props.query.id} />
-    </PleaseSignin>
-);
+// get the page query from router
+import { useRouter } from 'next/router';
+
+// use these for SSR of the item
+import { SINGLE_ITEM_QUERY } from '../components/SingleItem';
+import { initializeApollo, addApolloState } from '../lib/apollo';
+
+const Update = props => {
+    // get the id from the url
+    const router =  useRouter();
+    const id = router.query.id || "";
+    return(
+        <PleaseSignin>
+            <UpdateItem id={id} />
+        </PleaseSignin>
+    )
+};
+
+// TODO: add SSR from item.js
 
 export default Update;
