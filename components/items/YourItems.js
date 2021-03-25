0e5f88776bc6accbca424f69709d6faadcbd0442
peter@lidee.be <peter@lidee.be>
2021-03-25 17:47:57 +0100

    updated YourItems

--- components/items/YourItems.js
+++ components/items/YourItems.js
@@ -1,55 +1,56 @@
-//import { Query } from 'react-apollo';
+// this component displays the users items
+// it get the items from useritemscontext, no ssr (yet?)
+
+import { useContext } from 'react';
 import Link from 'next/link';
 
-import User from './account/User';
+import UserItemsContext from '../context/UserItemsContext';
+import MetaTitle from '../snippets/MetaTitle';
+import Loader from '../snippets/Loader';
+import Error from '../snippets/Error';
+import NoData from '../snippets/NoData';
+import UploadButton from '../snippets/UploadButton';
+import Item from '../item/Item';
 
-import Item from './Item';
-import MetaTitle from './snippets/MetaTitle';
-import Loader from './snippets/Loader';
-import Error from './snippets/Error';
-import UploadButton from './snippets/UploadButton';
+function YourItemsWrap(){
+    return(
+        <section>
+            <MetaTitle>Your pics</MetaTitle>
+            <h1 className="title">Your pics</h1>
+            <YourItems />
+        </section>
+    )
+}
 
-const MyItems = props => (
-    <section>
-        <MetaTitle>Your pics</MetaTitle>
-        <h1 className="title">Your pics</h1>
-        <User>
-            {({ loading, error, data }) => {
-                if(loading) return <Loader containerClass="items-loader" />;                
-                if(error) return <Error error={error} />
-                if(!data.me) return <p className="no-data">Uhm, something went wrong :/. Try reloading the page.</p>;
-                const { me } = data;
+function YourItems(){
+    // get userItems context
+    const { error, data, loading } = useContext(UserItemsContext);
+    if(loading) return <Loader containerClass="items-loader" />
+    if(error) return <Error error={error} />
+    if(!data.userItems) return <NoData>Uhm, something went wrong :/. Try reloading the page.</NoData>;
+    if(!data.userItems.length){
+        return <NoData>Looks like you haven't uploaded any pics yet. Maybe <Link href="/addapicture"><a>upload your first pic</a></Link>?</NoData>
+    }
+    const message = "You can edit or delete your uploaded pics here."
+    return(
+        <>
+            {data.userItems.length == 10 && 
+                <p className="items__message">{message} You used all your uploads. We like that.</p>}
+            {data.userItems.length < 10 &&
+                <p className="items__message">{message} You have {10 - data.userItems.length} uploads left.</p>}
+            {data.userItems.length > 0 &&
+                <div className="grid-items">
+                    {data.userItems.map(item => <Item key={item.id} item={item} showEdit={true} hideVote={true} /> )}
+                </div>
+            }
+            {data.userItems.length < 10 &&
+                <p className="no-data extra-margin">
+                    <span style={{"paddingRight": ".5em"}}>You have {10 - data.userItems.length} uploads left.</span>
+                    <UploadButton />
+                </p>
+            }
+        </>
+    )
+}
 
-                if(!me.items.length){
-                    return <p className="no-data">Looks like you haven't uploaded any pics yet. Maybe <Link href="/addapicture"><a>upload your first pic</a></Link>?</p>
-                }
-
-                const message = "You can edit or delete your uploaded pics here."
-
-                return(
-                    <>
-                        {me.items.length == 10 && 
-                            <p className="items__message">{message} You used all your uploads. We like that.</p>}
-                        {me.items.length < 10 &&
-                            <p className="items__message">{message} You have {10 - me.items.length} uploads left.</p>}
-
-                        {me.items.length > 0 &&
-                            <div className="grid-items">
-                                {me.items.map(item => <Item key={item.id} item={item} showEdit={true} hideVote={true} /> )}
-                            </div>
-                        }
-                        {me.items.length < 10 &&
-                            <p className="no-data extra-margin">
-                                <span style={{"paddingRight": ".5em"}}>You have {10 - me.items.length} uploads left.</span>
-                                <UploadButton />
-                            </p>
-                        }
-                    </>
-                )
-
-            }}
-        </User>
-    </section>
-)
-
-export default MyItems;
+export default YourItemsWrap;
