36bb9b0b5af0ecd66fa6aae457f2ad4d5328b9a0
peter@lidee.be <peter@lidee.be>
2021-04-27 22:14:18 +0200

    expanded tag query with id

--- queriesAndMutations/tags/tagQueries.js
+++ queriesAndMutations/tags/tagQueries.js
@@ -2,8 +2,8 @@ import { gql } from '@apollo/client';
 
 // used by /tags/tagname and /admin/tags/tagname
 const SINGLE_TAG_QUERY = gql`
-    query($tagSlug: String!){
-        tag( tagSlug: $tagSlug ){
+    query($tagSlug: String, $tagId: ID){
+        tag( tagSlug: $tagSlug, tagId: $tagId ){
             id
             name
             slug
