bed1cc91752fae4c5f5c6bb5aa8d967931059dff
peter@lidee.be <peter@lidee.be>
2021-05-06 17:14:06 +0200

    Moved queries

--- components/items/SingleTaxonomyQueries.js
+++ components/items/SingleTaxonomyQueries.js
@@ -6,15 +6,7 @@ import {gql} from '@apollo/client';
 import { ITEM_FIELDS_FRAGMENT } from '../../gqlFragments/itemFragment';
 import { perPage } from '../../config';
 
-const TAG_COUNT_QUERY = gql`
-    query TAG_COUNT_QUERY($tagSlug: String!){
-        itemsConnection( tagSlug: $tagSlug ){
-            aggregate{
-                count
-            }
-        }
-    }
-`;
+
 
 const TAG_ITEMS_QUERY = gql`
     query TAG_ITEMS_QUERY(
@@ -33,16 +25,6 @@ const TAG_ITEMS_QUERY = gql`
         }
     }
     ${ITEM_FIELDS_FRAGMENT}
-`;
-
-const LOCATION_COUNT_QUERY = gql`
-    query LOCATION_COUNT_QUERY($countryCode: String!, $locationSlug: String!){
-        itemsConnection( locationSlug: $locationSlug, countryCode: $countryCode ){
-            aggregate{
-                count
-            }
-        }
-    }
 `;
 
 const LOCATION_ITEMS_QUERY = gql`
@@ -76,16 +58,6 @@ const COUNTRY_EXISTS_QUERY = gql`
     }
 `;
 
-const COUNTRY_COUNT_QUERY = gql`
-    query COUNTRY_COUNT_QUERY($countryCode: String!){
-        itemsConnection( countryCode: $countryCode ){
-            aggregate{
-                count
-            }
-        }
-    }
-`;
-
 const COUNTRY_ITEMS_QUERY = gql`
     query COUNTRY_ITEMS_QUERY(
             $countryCode: String!, 
@@ -106,11 +78,8 @@ const COUNTRY_ITEMS_QUERY = gql`
 `;
 
 export { 
-    TAG_COUNT_QUERY,
     TAG_ITEMS_QUERY,
-    LOCATION_COUNT_QUERY,
     LOCATION_ITEMS_QUERY,
     COUNTRY_EXISTS_QUERY,
-    COUNTRY_COUNT_QUERY,
     COUNTRY_ITEMS_QUERY
 };
