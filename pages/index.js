b27d4bc6fdb4aa4f610a454d2f0b2114cf51af8e
peter@lidee.be <peter@lidee.be>
2021-05-10 12:06:05 +0200

    fixed ssr for 2 queries (didn't have vars)

--- pages/index.js
+++ pages/index.js
@@ -2,16 +2,16 @@ import { initializeApollo, addApolloState } from '../lib/apollo';
 import { ITEMS_QUERY } from '../queriesAndMutations/items/itemQueries'
 import Home from '../components/Home';
 
-
 const Index = props => <Home />
 
 // this function only runs on the server by Next.js
 export async function getServerSideProps({params, query}){
     const apolloClient = initializeApollo()
     
-    // TODO: fix these queries, need vars?
-    await apolloClient.query({ query: ITEMS_QUERY }).catch(error => console.warn(error.message))
-    await apolloClient.query({ query: ITEMS_QUERY }).catch(error => console.warn(error.message))
+    // query recent items
+    await apolloClient.query({ query: ITEMS_QUERY, variables: { orderBy: "createdAt_DESC" }}).catch(error => console.warn(error.message))
+    // query most voted items
+    await apolloClient.query({ query: ITEMS_QUERY, variables: { orderBy: "voteCount_DESC" }}).catch(error => console.warn(error.message))
 
     return addApolloState(apolloClient, {
         props: {},
