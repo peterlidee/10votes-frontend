abcddb142566c67cba5c1b0e5fbc0bc1a2fa32d4
peter@lidee.be <peter@lidee.be>
2021-05-13 15:52:14 +0200

    Removed ssr query

--- pages/admin/user.js
+++ pages/admin/user.js
@@ -1,5 +1,4 @@
 import { initializeApollo, addApolloState } from '../../lib/apollo'
-import { SINGLE_USER_QUERY } from '../../queriesAndMutations/users/userQueries'
 import AdminGate from "../../components/admin/AdminGate"
 import EditUser from "../../components/admin/EditUser"
 
@@ -13,16 +12,10 @@ function UsersPage(props){
 
 // https://github.com/vercel/next.js/blob/canary/examples/with-apollo/pages/index.js
 // this will make a server-side request 
+// we don't query cause user logged in doesn't work ssr
 export async function getServerSideProps(context) {
     const apolloClient = initializeApollo()
     const id = context.query.id || "";
-
-    if(id){
-        await apolloClient.query({
-            query: SINGLE_USER_QUERY,
-            variables: { userId: id },
-        }).catch(error => console.warn(error.message))
-    }
   
     return addApolloState(apolloClient, {
         props: { userId: id },
