f4ad8d1e1d3b6006f9a243b51f785be924b0770f
peter@lidee.be <peter@lidee.be>
2021-05-14 12:16:13 +0200

    Added back to admin link

--- components/taxonomy/SingleTaxonomyAdmin.js
+++ components/taxonomy/SingleTaxonomyAdmin.js
@@ -10,12 +10,21 @@ import PropTypes from 'prop-types';
 import Loader from '../snippets/Loader';
 import Error from '../snippets/Error';
 import NoData from '../snippets/NoData';
+import Link from 'next/link';
 
 const SingleTaxonomyAdmin = (props) => { // props: tagId and type (locations, tags, users)
     // construct the correct key from props.type, f.e. tagId or locationId or userId
     const key = `${props.type.slice(0,-1)}Id`;
     // check if there's an Id prop or if it's empty
-    if(!props[key]) return <NoData>No {props.type.slice(0,-1)} found.</NoData>
+    if(!props[key]) return(
+        <NoData>
+            No {props.type.slice(0,-1)} found.
+            <Link href="/admin" >
+                <a className="no-data__link">&lt; back to admin</a>
+            </Link>    
+        </NoData>
+    )
+    
     // get the relevant query and variables
     const { query, variables } = getQueriesAndVariables(props.type, 'single', {[key]: props[key]});
     // make the query
@@ -24,7 +33,14 @@ const SingleTaxonomyAdmin = (props) => { // props: tagId and type (locations, ta
 
     if(loading) return <Loader containerClass="items-loader" />
     if(error) return <Error error={error} />
-    if(!data || !data[props.type.slice(0,-1)]) return <NoData>No {props.type.slice(0,-1)} found.</NoData>
+    if(!data || !data[props.type.slice(0,-1)]) return(
+        <NoData>
+            No {props.type.slice(0,-1)} found.
+            <Link href="/admin" >
+                <a className="no-data__link">&lt; back to admin</a>
+            </Link>    
+        </NoData>
+    )
 
     return props.children(data);
 }
