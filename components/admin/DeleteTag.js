5955c17e521313d16002d72a87489fe03624c0a7
peter@lidee.be <peter@lidee.be>
2021-05-18 20:32:17 +0200

    Made deleteTag work for locations too

--- components/admin/DeleteTag.js
+++ components/admin/DeleteTag.js
@@ -2,40 +2,49 @@ import Router from 'next/router'
 
 import { useMutation } from '@apollo/client'
 import { DELETE_TAG_MUTATION } from '../../queriesAndMutations/tags/tagMutations'
+import { DELETE_LOCATION_MUTATION } from '../../queriesAndMutations/locations/locationMutations'
 
 import FormRow from '../formParts/FormRow'
 import FormButton from '../formParts/FormButton'
 import Error from '../snippets/Error'
 
 import PropTypes from 'prop-types'
+import capitalizeString from '../../lib/capitalizeString'
+
+const query = {
+    tags: DELETE_TAG_MUTATION,
+    locations: DELETE_LOCATION_MUTATION,
+}
 
 const DeleteTag = (props) => {
 
-    const [ deleteTag, { loading, error } ] = useMutation(DELETE_TAG_MUTATION, { 
-        variables: { tagId: props.tagId },
+    const [deleteTaxonomy, { loading, error }] = useMutation(query[props.type], { 
+        variables: { [`${props.type.slice(0,-1)}Id`]: props.id },
     });
 
     const handleDelete = (e) => {
         // prevent form submit
         e.preventDefault();
 
-        if(confirm('Are you sure you want to delete this tag?')){
-            deleteTag()
+        if(confirm(`Are you sure you want to delete this ${props.type.slice(0,-1)}?`)){
+            deleteTaxonomy()
                 .then(res => {
+                    console.log('res',res)
                     // delete tag was succesfull
-                    if(res.data.deleteTag){
+                    if(res.data[`delete${capitalizeString(props.type.slice(0,-1))}`]){
                         // redirect
                         Router.push('/admin');
                     }
                 })
-                .catch(error => console.error('deleteTag error', error, error.message));
+                .catch(error => console.error('deleteTaxonomy error', error.message));
         }
     }
 
     return(
         <div className="admin-section">
             <h2 className="item-crud__title title">Delete</h2>
-            <p className="crud-message">Remove the tag from the database and from all items.</p>
+            {props.type == "tags" && <p className="crud-message">Remove the tag from the database and from all items.</p>}
+            {props.type == "locations" && <p className="crud-message">Remove the location from the database. You can only do this if the location has no items.</p>}
             <form 
                 onSubmit={handleDelete} 
                 className="form-part"
@@ -63,7 +72,8 @@ const DeleteTag = (props) => {
 }
 
 DeleteTag.propTypes = {
-    tagId: PropTypes.string.isRequired,
+    id: PropTypes.string.isRequired,
+    type: PropTypes.string.isRequired,
 };
 
 export default DeleteTag;
