ea0860f279503d376cea0847985ea14c8160b40a
peter@lidee.be <peter@lidee.be>
2021-05-31 15:07:00 +0200

    Added no pic option

--- components/admin/UsersItems.js
+++ components/admin/UsersItems.js
@@ -1,8 +1,10 @@
 import Item from '../item/Item';
+import NoData from '../snippets/NoData'
 
 const UsersItems = props => (
     <section>
         <h1 className="title">Users' pics</h1>
+        {props.items.length == 0 && <NoData>The user has not uploaded any pics.</NoData>}
         <div className="grid-items">
             {props.items.map(item => <Item key={item.id} item={item} showEdit={true} hideVote={true} /> )}
         </div>
