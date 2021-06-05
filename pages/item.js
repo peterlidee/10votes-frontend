3349941148c5ff07a777853320147fb5e94791ac
peter@lidee.be <peter@lidee.be>
2021-06-05 18:39:45 +0200

    Removed query from item.js, only query left

--- pages/item.js
+++ pages/item.js
@@ -1,25 +1,13 @@
 import SingleItem from '../components/item/SingleItem';
-import { SINGLE_ITEM_QUERY } from '../queriesAndMutations/items/itemQueries'
 import { initializeApollo, addApolloState } from '../lib/apollo';
 
-const ItemPage = (props) => {
-    return <SingleItem itemId={props.itemId} />
-}
+const ItemPage = (props) => <SingleItem itemId={props.itemId} />
 
-// https://github.com/vercel/next.js/blob/canary/examples/with-apollo/pages/index.js
-// this will make a server-side request 
-export async function getServerSideProps(context) {
+//https://github.com/vercel/next.js/blob/canary/examples/with-apollo/pages/index.js
+//this will make a server-side request 
+export async function getServerSideProps({ query }) {
     const apolloClient = initializeApollo()
-
-    const id = context.query.id || "";
-
-    if(id){
-        await apolloClient.query({
-            query: SINGLE_ITEM_QUERY,
-            variables: { itemId: id },
-        }).catch(error => console.warn(error.message))
-    }
-  
+    const id = query.id || ""; 
     return addApolloState(apolloClient, {
         props: { itemId: id },
     })
