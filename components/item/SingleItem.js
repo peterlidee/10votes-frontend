ff45040d0361f658f04fb15f42a70c2b169bd436
peter@lidee.be <peter@lidee.be>
2021-06-06 15:46:32 +0200

    Replaced previousData with nextFetchPolicy

--- components/item/SingleItem.js
+++ components/item/SingleItem.js
@@ -1,26 +1,22 @@
 import { useQuery } from '@apollo/client'
 import { SINGLE_ITEM_QUERY } from '../../queriesAndMutations/items/itemQueries'
 
-import PropTypes from 'prop-types';
-
 import Loader from '../snippets/Loader';
 import Error from '../snippets/Error';
 import NoData from '../snippets/NoData';
 import Item from './Item';
 
+import PropTypes from 'prop-types';
+
 function SingleItem(props){
     if(!props.itemId) return <NoData>No such picture found.</NoData>
-    const { error, loading, data, previousData } = useQuery(SINGLE_ITEM_QUERY, {
+    const { error, loading, data } = useQuery(SINGLE_ITEM_QUERY, {
         variables: { itemId: props.itemId }, 
-        fetchPolicy: "cache-and-network"
+        fetchPolicy: "cache-and-network",
+        nextFetchPolicy: "cache-only"
     });
-
-    if(loading && !previousData) return <Loader containerClass="items-loader" />
+    if(loading) return <Loader containerClass="items-loader" />
     if(error) return <Error error={error} />
-    if(previousData && !data){
-        if(!previousData.item) return <NoData>No such picture found.</NoData>
-        if(loading) return <Item item={previousData.item} single={true} />
-    }
     if(!data || !data.item) return <NoData>No such picture found.</NoData>    
     return <Item item={data.item} single={true} />
 }
