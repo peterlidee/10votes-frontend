f24ee250c73e7f21493d4b9c4536eae8c93be6a6
peter@lidee.be <peter@lidee.be>
2021-06-06 17:36:34 +0200

    Removed ssr query

--- pages/update.js
+++ pages/update.js
@@ -1,8 +1,6 @@
 import PleaseSignin from '../components/account/PleaseSignin';
 import UpdateItem from '../components/item/UpdateItem';
 
-// use these for SSR of the item
-import { SINGLE_ITEM_QUERY } from '../queriesAndMutations/items/itemQueries'
 import { initializeApollo, addApolloState } from '../lib/apollo';
 import CurrentUserItems from '../components/items/CurrentUserItems';
 
@@ -18,16 +16,7 @@ const Update = props => {
 
 export async function getServerSideProps(context) {
     const apolloClient = initializeApollo()
-
     const id = context.query.id || "";
-
-    if(id){
-        await apolloClient.query({
-            query: SINGLE_ITEM_QUERY,
-            variables: { itemId: id },
-        }).catch(error => console.warn(error.message))
-    }
-  
     return addApolloState(apolloClient, {
         props: { itemId: id },
     })
