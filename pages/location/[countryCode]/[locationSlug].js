49aa87d2eb1a3fa40856ebd7dba6a1dbe22d0159
peter@lidee.be <peter@lidee.be>
2021-06-06 18:32:10 +0200

    removed ssr queries

--- pages/location/[countryCode]/[locationSlug].js
+++ pages/location/[countryCode]/[locationSlug].js
@@ -1,9 +1,8 @@
 // displays location, fe .../location/be/gent
 
-import { initializeApollo, addApolloState } from '../../../lib/apollo';
+import { initializeApollo, addApolloState } from '../../../lib/apollo'
 import verifyOrderParam from '../../../lib/verifyOrderParam'
-import SingleTaxonomyExists from '../../../components/taxonomy/SingleTaxonomyExists';
-import { LOCATIONS_QUERY } from '../../../queriesAndMutations/locations/locationQueries'
+import SingleTaxonomyExists from '../../../components/taxonomy/SingleTaxonomyExists'
 
 const LocationPage = props => (
     <SingleTaxonomyExists type="locations" {...props} />
@@ -19,13 +18,6 @@ export async function getServerSideProps({params, query}){
     const countryCode = params.countryCode;
     const orderBy = verifyOrderParam(query.orderBy);
     const page = +query.page || 1;
-    
-    if(countryCode && locationSlug){
-        await apolloClient.query({
-            query: LOCATIONS_QUERY,
-            variables: { locationSlug: locationSlug, countryCode: countryCode },
-        }).catch(error => console.warn(error.message))
-    }
 
     return addApolloState(apolloClient, {
         props: { locationSlug, countryCode, orderBy, page },
--- pages/location/[countryCode]/index.js
+++ pages/location/[countryCode]/index.js
@@ -1,9 +1,8 @@
 // displays country, fe .../location/be
 
-import { initializeApollo, addApolloState } from '../../../lib/apollo';
+import { initializeApollo, addApolloState } from '../../../lib/apollo'
 import verifyOrderParam from '../../../lib/verifyOrderParam'
-import SingleTaxonomyExists from '../../../components/taxonomy/SingleTaxonomyExists';
-import { SINGLE_COUNTRY_QUERY } from '../../../queriesAndMutations/countries/countryQueries'
+import SingleTaxonomyExists from '../../../components/taxonomy/SingleTaxonomyExists'
 
 const CountryPage = props => (
     <SingleTaxonomyExists type="country" {...props} />
@@ -18,13 +17,6 @@ export async function getServerSideProps({params, query}){
     const countryCode = params.countryCode;
     const orderBy = verifyOrderParam(query.orderBy);
     const page = +query.page || 1;
-    
-    if(countryCode){
-        await apolloClient.query({
-            query: SINGLE_COUNTRY_QUERY,
-            variables: { countryCode: countryCode },
-        }).catch(error => console.warn(error.message))
-    }
 
     return addApolloState(apolloClient, {
         props: { countryCode, orderBy, page },
