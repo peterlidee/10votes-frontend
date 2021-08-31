583ff0975190aa80cd544a3dcd635b102091dd43
peter@lidee.be <peter@lidee.be>
2021-08-31 18:06:36 +0200

    New back-end url

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
 // This is client side config only - don't put anything in here that shouldn't be public!
 export const endpoint = `http://localhost:4444`;
-export const prodEndPoint = 'https://tenvotes-yoga-prod.herokuapp.com';
+//export const prodEndPoint = 'https://tenvotes-yoga-prod.herokuapp.com';
+export const prodEndPoint = 'http://api.10votes.be';
 export const perPage = 10;
